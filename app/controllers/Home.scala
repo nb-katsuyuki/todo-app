1aa03c973ef75a338b10f24b439fefaed5f7f34b
katsuyuki suzuki <katsuyuki.suzuki@nextbeat.net>
2021-11-15 13:23:23 +0900

    お昼コミ TODOモデル実装＋スタブによりリスト表示

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -6,7 +6,7 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-
+import lib.model.Todo
 import model.ViewValueHome
 
 @Singleton
@@ -17,7 +17,15 @@ class HomeController @Inject() (val controllerComponents: ControllerComponents)
     val vv = ViewValueHome(
       title = "Home",
       cssSrc = Seq("main.css"),
-      jsSrc = Seq("main.js")
+      jsSrc = Seq("main.js"),
+      // @TODO とりあえずスタブ
+      todoes = (1 to 10).map(i =>
+        Todo(
+          title = s"title${i}",
+          body = Some(s"body${i}"),
+          state = Todo.Status.PENDING
+        )
+      )
     )
     Ok(views.html.Home(vv))
   }
--- app/model/Home.scala
+++ app/model/Home.scala
@@ -1,15 +1,15 @@
 /**
- *
  * to do sample project
- *
  */
 
 package model
 
+import lib.model.Todo
+
 // Topページのviewvalue
 case class ViewValueHome(
-  title:  String,
-  cssSrc: Seq[String],
-  jsSrc:  Seq[String],
+    title: String,
+    cssSrc: Seq[String],
+    jsSrc: Seq[String],
+    todoes: Seq[Todo.WithNoId] // @TODO とりあえず
 ) extends ViewValueCommon
-
